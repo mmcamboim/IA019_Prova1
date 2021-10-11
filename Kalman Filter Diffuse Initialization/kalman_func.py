a0ccff9fb5b55282e77cb2e93790134087842961
mcamboim <mcamboim@cpqd.com.br>
2021-10-11 10:02:09 -0300

    Adicionando a inicialização Difusa

---
+++ Kalman Filter Diffuse Initialization/kalman_func.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  7 19:17:35 2021
+
+@author: mcamboim
+"""
+import numpy as np
+
+class KalmanFilter:
+    def __init__(self,Ak,Bk,Ck,W_STD,V_STD,ITER,Pk0,xk0):
+        self.InitTIVar(Ak,Bk,Ck,W_STD,V_STD)
+        self.InitTVVar(Ak,Bk,Ck,ITER)
+        self.VarInit(Ak,Pk0,xk0)
+        self.VarRefinementInit(Ak,ITER)
+
+    def InitTIVar(self,Ak,Bk,Ck,W_STD,V_STD):
+        self.Ak = Ak
+        self.Bk = Bk
+        self.Ck = Ck
+        self.Qk = np.eye(Bk.shape[1]) * W_STD
+        self.Rk = np.eye(Ck.shape[0]) * V_STD
+
+    def InitTVVar(self,Ak,Bk,Ck,ITER):
+        self.Lk = np.zeros((Ak.shape[0],Ak.shape[1],ITER + 1))
+        self.Mk = np.zeros((Ak.shape[0],Ck.shape[0],ITER + 1))
+        self.Fk = np.zeros((Ck.shape[0],Ck.shape[0],ITER + 1))
+        self.Kk = np.zeros((Ak.shape[0],Ck.shape[0],ITER + 1))
+        self.Pk = np.zeros((Ak.shape[0],Ak.shape[1],ITER + 1))
+        self.xk = np.zeros((Ak.shape[0],1,ITER + 1))
+        self.yk = np.zeros((Ck.shape[0],1,ITER + 1))
+        self.ek = np.zeros((Ck.shape[0],1,ITER + 1))
+
+    def VarInit(self,Ak,Pk0,xk0):
+        self.xk[:,:,0] = xk0
+        self.Pk[:,:,0] = np.eye(Ak.shape[0]) * Pk0
+        self.iter = 0
+        
+    def VarRefinementInit(self,Ak,ITER):
+        self.rk = np.zeros((Ak.shape[0],1,ITER))
+        self.Nk = np.zeros((Ak.shape[0],Ak.shape[1],ITER))
+        self.xk_N = np.zeros((Ak.shape[0],1,ITER + 1))
+        self.Pk_N = np.zeros((Ak.shape[0],Ak.shape[1],ITER + 1))
+
+    def RunKfOneStepAhead(self,y_meas):
+        self.Mk[:,:,self.iter] = self.Ak @ self.Pk[:,:,self.iter] @ self.Ck.T
+        self.Fk[:,:,self.iter] = self.Ck @ self.Pk[:,:,self.iter] @ self.Ck.T + self.Rk
+        self.Kk[:,:,self.iter] = self.Mk[:,:,self.iter] @ np.linalg.inv(self.Fk[:,:,self.iter])
+
+        self.yk[:,:,self.iter] = self.Ck @ self.xk[:,:,self.iter]
+        self.ek[:,:,self.iter] = y_meas - self.yk[:,:,self.iter]
+        
+        vt = self.Kk[:,:,self.iter] @ self.ek[:,:,self.iter]
+        self.xk[:,:,self.iter + 1] = self.Ak @ self.xk[:,:,self.iter]  +  vt
+
+        self.Lk[:,:,self.iter] = self.Ak - self.Kk[:,:,self.iter ] @ self.Ck
+
+        self.Pk[:,:,self.iter + 1] = self.Ak @ self.Pk[:,:,self.iter] @ self.Lk[:,:,self.iter].T + self.Bk @ self.Qk @ self.Bk.T
+
+        self.iter +=1
+        
+    def RunKfRefinement(self):
+        self.rk[:,:,self.iter - 1] = self.Ck.T @ np.linalg.inv(self.Fk[:,:,self.iter]) @ self.ek[:,:,self.iter] + self.Lk[:,:,self.iter].T @ self.rk[:,:,self.iter] 
+        self.Nk[:,:,self.iter - 1] = self.Ck.T @ np.linalg.inv(self.Fk[:,:,self.iter]) @ self.Ck + self.Lk[:,:,self.iter].T @ self.Nk[:,:,self.iter] @ self.Lk[:,:,self.iter]
+        self.xk_N[:,:,self.iter] = self.xk[:,:,self.iter] + self.Pk[:,:,self.iter] @ self.rk[:,:,self.iter - 1]
+        self.Pk_N[:,:,self.iter] = self.Pk[:,:,self.iter] - self.Pk[:,:,self.iter] @ self.Nk[:,:,self.iter - 1] @ self.Pk[:,:,self.iter]
+        
+        self.iter -= 1
+        # A
+        
+        
---
+++ Kalman Filter Diffuse Initialization/main.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  7 19:15:23 2021
+
+@author: mcamboim
+"""
+
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+from kalman_func import KalmanFilter
+
+plt.close('all')
+
+# Loading System Data ========================================================
+with open('system.pickle','rb') as file:
+    [Ak,Bk,Ck,xk_t,yk_t,W_STD,V_STD,ITER] = pickle.load(file)
+
+# Initial Variables ==========================================================
+Pk0 = 1.0
+xk0 = np.array([np.pi/18,0]).reshape(2,1)
+kf = KalmanFilter(Ak,Bk,Ck,W_STD,V_STD,ITER,Pk0,xk0)
+
+# Running ====================================================================
+for i in range(ITER):
+    kf.RunKfOneStepAhead(yk_t[i].reshape(2,1))
+# Refinement =================================================================
+
+kf.iter = ITER - 1
+for i in range(ITER - 1):
+    kf.RunKfRefinement()
+    
+
+# Plotting ===================================================================
+plt.figure(figsize=(8,8),dpi=180)
+plt.subplot(2,1,1)
+plt.plot(range(0,500),xk_t[:,0],c='b',lw=2)
+plt.plot(range(0,501),kf.xk[0,0,:],c='r',lw=2)
+plt.plot(range(0,501),kf.xk_N[0,0,:],c='g',lw=2)
+plt.legend(['$xk_{1real}$','$xk_{1est}$'])
+plt.xlim([0,ITER])
+plt.grid(True,ls='dotted')
+plt.ylabel('xk_1 []')
+
+plt.subplot(2,1,2)
+plt.plot(range(0,500),xk_t[:,1],c='b',lw=2)
+plt.plot(range(0,501),kf.xk[1,0,:],c='r',lw=2)
+plt.plot(range(0,501),kf.xk_N[1,0,:],c='g',lw=2)
+plt.legend(['$xk_{2real}$','$xk_{2est}$'])
+plt.xlim([0,ITER])
+plt.grid(True,ls='dotted')
+plt.xlabel('Iteração [N]')
+plt.ylabel('xk_2 []')
+
+print(np.sqrt(np.mean((xk_t[:,0] - kf.xk[0,0 ,:-1])**2)))
+print(np.sqrt(np.mean((xk_t[:,0] - kf.xk_N[0,0 ,:-1])**2)))
+
+print(np.sqrt(np.mean((xk_t[:,1] - kf.xk[1,0 ,:-1])**2)))
+print(np.sqrt(np.mean((xk_t[:,1] - kf.xk_N[1,0 ,:-1])**2)))
+
+plt.figure()
+plt.plot(kf.Pk[1,1,:])
+plt.plot(kf.Pk_N[1,1,:])
